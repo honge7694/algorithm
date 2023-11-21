e192851a09e8fbe9c305aeabdb28c7fc5dda7de0
hongui <honge7694@naver.com>
2023-11-21 09:26:59 +0900

    baekjoon 1874_스택수열

--- baekjoon/1874_스택수열re.py
+++ baekjoon/1874_스택수열re.py
@@ -1,28 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
+N = int(input())
 nums = [int(input()) for _ in range(N)]
 
-result = []
-stack = []
-count = 1
+result = [] # +, -를 담을 리스트
+stack = [] # num을 담을 리스트
+count = 1 # 수열의 수와 스택에 담긴 값 비교할 count
 temp = True
 
 for num in nums:
-    while count <= num:
-        stack.append(count)
-        result.append("+")
-        count += 1
+	# 반복되는 수열의 수는 담지않도록 count <= num을 담음
+	while count <= num:
+		stack.append(count)
+		result.append('+')
+		count += 1
     
-    if num == stack[-1]:
-        stack.pop()
-        result.append("-")
-    else:
-        temp = False
+	if num == stack[-1]: # 입력한 값과 스택의 마지막 값 비교
+		stack.pop()
+		result.append('-')
+	else:
+		temp = False
+		break
 
-if temp:
-    for i in result:
-        print(i)
+if temp == True:
+	[print(i) for i in result]
 else:
-    print("NO")
+	print("NO")
