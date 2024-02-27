bf8f23c2cdb96ac7a232b3e80db783ce66f6ace6
honge7694 <76715487+honge7694@users.noreply.github.com>
2024-02-27 22:33:25 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean sosu(int number) {
-        for (int i = 2; i < number; i++) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
                 return false;
             }
